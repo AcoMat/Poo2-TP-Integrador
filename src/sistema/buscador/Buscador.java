ecad6fed57a418d7aeee3b154ec6124a8fd78de0
AcoMat <aaco.maty@gmail.com>
2023-05-24 20:52:56 -0300

    Agrego archivos faltantes

---
+++ src/sistema/buscador/Buscador.java
@@ -0,0 +1,13 @@
+package sistema.buscador;
+
+import sistema.muestras.Muestra;
+
+import java.util.ArrayList;
+
+public class Buscador {
+    private ArrayList<Muesta> muestasTotalesDelSys;
+
+    public void nuevaMuestraEnSistema(Muestra m){
+
+    }
+}
---
+++ src/sistema/webSite/Website.java
@@ -0,0 +1,5 @@
+package sistema.webSite;
+
+public class Website {
+    //registrar usuarios usar el buscador etc
+}
