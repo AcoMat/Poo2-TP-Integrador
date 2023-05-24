bc794f3c1740e897847b3fd938890a6681870f1e
AcoMat <aaco.maty@gmail.com>
2023-05-24 20:25:15 -0300

    Logica postear muestras

--- src/app/usuario/Usuario.java
+++ src/app/usuario/Usuario.java
@@ -1,12 +1,17 @@
 package app.usuario;
 
+import app.buscador.Buscador;
 import app.muestras.Muestra;
 import app.sistemaDeVotos.*;
+import app.ubicacion.Ubicacion;
+
+import java.util.ArrayList;
 
 public class Usuario {
 
 	private String userName;
 	private Tipo state;
+	private ArrayList<Muestra> muestrasPosteadas;
 
 	// Constructor
 
@@ -20,7 +25,7 @@ public class Usuario {
 		}
 	}
 
-	// Setter
+	// Setter y Getters
 
 	private void setTipo(Tipo state) {
 		this.state = state;
@@ -31,12 +36,21 @@ public class Usuario {
 		this.userName = userName;
 	}
 
+	public String getUserName() {
+		return userName;
+	}
+
 	// Metodos
 
 	public void opinar(TipoDeVoto voto, Muestra muestra) {
 		this.state.opinar(voto, muestra);
 	}
 
+	public void postearMuestra(TipoDeVoto opinion, String fotoURL, Ubicacion ubicacion){
+		Muestra m = new Muestra(opinion, fotoURL, this, ubicacion);
+		muestrasPosteadas.add(m);
+
+	}
 
 
 }
