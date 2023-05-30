32557005f23b0ec262537fb4fcdc656366338c3c
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2023-05-30 00:12:31 -0300

    Ajustes para prolijidad

--- src/sistema/usuario/Nivel.java
+++ src/sistema/usuario/Nivel.java
@@ -7,22 +7,16 @@ import sistema.muestras.*;
 
 public abstract class Nivel {
 
-	public Usuario usuario;
-
-	public void opinar(Muestra muestra, TipoDeVoto voto) {
+	public void opinar(Usuario usuario, Muestra muestra, TipoDeVoto voto) {
 		Opinion nuevaOpinion = new Opinion(voto, usuario.getUserName(), usuario.esExperto());
 		muestra.getHandler().nuevaOpinion(usuario, voto);
 		usuario.guardarOpinion(muestra, nuevaOpinion);
 	};
 
-	public void enviarMuestra(TipoDeVoto especie, String fotoURL, Ubicacion ubicacion) {
+	public void enviarMuestra(Usuario usuario, TipoDeVoto especie, String fotoURL, Ubicacion ubicacion) {
 		Muestra nuevaMuestra = new Muestra(especie, fotoURL, usuario, ubicacion);
 		usuario.guardarMuestra(nuevaMuestra);
 	};
-
-	public void setUsuario(Usuario nuevoUsuario) {
-		this.usuario = nuevoUsuario;
-	}
 
 	abstract boolean esExperto();
 }
--- src/sistema/usuario/Usuario.java
+++ src/sistema/usuario/Usuario.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import sistema.sistemaDeVotos.Opinion;
 import sistema.sistemaDeVotos.TipoDeVoto;
+import sistema.ubicacion.Ubicacion;
 import sistema.webSite.Website;
 
 public class Usuario {
@@ -24,6 +25,48 @@ public class Usuario {
 		this.nivel = new Basico();
 	}
 
+	
+	//Getters
+	public String getUserName() {
+		return this.userName;
+	}
+
+	// Setters
+
+	private void setConocimientoValidado(boolean conocimientoValidado) {
+		this.conocimientoValidado = conocimientoValidado;
+	}
+	
+	public void setNivel() {
+		this.nivel = this.esExperto() ? new Experto() : new Basico();
+	}
+
+	// Metodos
+	
+	
+	//Opinar
+	public void opinar(Muestra muestra, TipoDeVoto voto) {
+		this.setNivel();
+		this.nivel.opinar(this, muestra, voto);
+	}
+
+	public void guardarOpinion(Muestra muestra, Opinion opinion) {
+		this.website.registrarOpinion(muestra, opinion);
+		this.opiniones.add(opinion);
+	}
+
+	//Enviar muestras
+	public void enviarMuestra(TipoDeVoto especie, String fotoURL, Ubicacion ubicacion) {
+		this.setNivel();
+		this.nivel.enviarMuestra(this, especie, fotoURL, ubicacion);
+	}
+
+	public void guardarMuestra(Muestra muestra) {
+		this.website.registrarPosteoDesdeLaApp(muestra);
+		this.muestras.add(muestra);
+	}
+	
+	//Calculos para saber si es experto
 	private long cantEnviosEn30Dias() {
 		Date fechaActual = new Date();
 		return muestras.stream().filter(muestra -> muestra.getFecha().getTime() - fechaActual.getTime() <= 30).count();
@@ -44,38 +87,6 @@ public class Usuario {
 
 	public Boolean esExperto() {
 		return this.conocimientoValidado || (this.cumpleEnvios() && this.cumpleRevisiones());
-	}
-
-	public void setNivel() {
-		this.nivel = this.esExperto() ? new Experto() : new Basico();
-		this.nivel.setUsuario(this);
-	}
-
-	// Setter
-
-	private void setConocimientoValidado(boolean conocimientoValidado) {
-		this.conocimientoValidado = conocimientoValidado;
-	}
-
-	// Metodos
-
-	public void opinar(Muestra muestra, TipoDeVoto voto) {
-		this.setNivel();
-		this.nivel.opinar(muestra, voto);
-	}
-
-	public void guardarOpinion(Muestra muestra, Opinion opinion) {
-		this.website.registrarOpinion(muestra, opinion);
-		this.opiniones.add(opinion);
-	}
-
-	public void guardarMuestra(Muestra muestra) {
-		this.website.registrarPosteoDesdeLaApp(muestra);
-		this.muestras.add(muestra);
-	}
-
-	public String getUserName() {
-		return this.userName;
 	}
 
 }
