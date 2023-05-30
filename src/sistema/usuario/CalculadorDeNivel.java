93c9aee76283173b41bf09ddad03f1add3176f21
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2023-05-30 00:42:01 -0300

    Se creó el calculador de nivel para mejor single responsibility

---
+++ src/sistema/usuario/CalculadorDeNivel.java
@@ -0,0 +1,27 @@
+package sistema.usuario;
+
+import java.util.Date;
+
+public class CalculadorDeNivel {
+	private long cantEnviosEn30Dias(Usuario usuario) {
+		Date fechaActual = new Date();
+		return usuario.getMuestras().stream().filter(muestra -> muestra.getFecha().getTime() - fechaActual.getTime() <= 30).count();
+	}
+
+	private long cantRevisionesEn30Dias(Usuario usuario) {
+		Date fechaActual = new Date();
+		return usuario.getOpiniones().stream().filter(opinion -> opinion.getFecha().getTime() - fechaActual.getTime() <= 30).count();
+	}
+
+	private Boolean cumpleEnvios(Usuario usuario) {
+		return this.cantEnviosEn30Dias(usuario) >= 10;
+	}
+
+	private Boolean cumpleRevisiones(Usuario usuario) {
+		return this.cantRevisionesEn30Dias(usuario) >= 20;
+	}
+
+	public Boolean esExperto(Usuario usuario) {
+		return usuario.conocimientoValidado() || (this.cumpleEnvios(usuario) && this.cumpleRevisiones(usuario));
+	}
+}
