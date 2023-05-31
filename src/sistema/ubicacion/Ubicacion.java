23623dbee2f10aede848e60db9278bbe57198125
AcoMat <aaco.maty@gmail.com>
2023-05-31 17:47:31 -0300

    distancia entre dos puntos

--- src/sistema/ubicacion/Ubicacion.java
+++ src/sistema/ubicacion/Ubicacion.java
@@ -9,9 +9,52 @@ public class Ubicacion {
 	double latitud;
 	double longitud;
 
-	public int distanciaHasta(Ubicacion ubi){
-		//TODO, teorema de pitagoras
-		return 0;
+	// Getters
+
+	public double getLatitud() {
+		return latitud;
+	}
+	public double getLongitud() {
+		return longitud;
+	}
+
+	//	Constructor
+
+	public Ubicacion(double latitud, double longitud) {
+		this.latitud = latitud;
+		this.longitud = longitud;
+	}
+
+	//
+
+	public double distanciaHasta(Ubicacion ubi){
+		//Teorema de pitagoras?? o Formula de Haversine ??
+
+		// Pitagoras:
+		double calculoXs = Math.pow((ubi.getLatitud()-this.getLatitud()),2);
+		double calculoYs = Math.pow((ubi.getLongitud()-this.getLongitud()),2);
+		return Math.sqrt(calculoXs + calculoYs);
+
+		/*	Haversine:
+
+			public final static double AVERAGE_RADIUS_OF_EARTH_KM = 6371;
+			public int calculateDistanceInKilometer(double userLat, double userLng,
+			double venueLat, double venueLng) {
+
+			double latDistance = Math.toRadians(userLat - venueLat);
+			double lngDistance = Math.toRadians(userLng - venueLng);
+
+			double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+					+ Math.cos(Math.toRadians(userLat)) * Math.cos(Math.toRadians(venueLat))
+					* Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
+
+			double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+			return (int) (Math.round(AVERAGE_RADIUS_OF_EARTH_KM * c));
+		}
+
+		*/
+
 	}
 
 	public ArrayList<Ubicacion> aquellasAMenosDe1Km(ArrayList<Ubicacion> ubicaciones){
