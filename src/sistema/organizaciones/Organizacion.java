d9b059c7378c654e4cf225ea90d80caa0251549e
AcoMat <aaco.maty@gmail.com>
2023-05-31 16:22:05 -0300

    inicializo la clase Organizacion

--- src/sistema/organizaciones/Organizacion.java
+++ src/sistema/organizaciones/Organizacion.java
@@ -1,4 +1,10 @@
 package sistema.organizaciones;
 
+import sistema.ubicacion.Ubicacion;
+
 public class Organizacion {
+    private Ubicacion ubicacion;
+    private TipoDeOrg tipoDeLaOrg;
+    private int cantEmpleados;
+
 }
