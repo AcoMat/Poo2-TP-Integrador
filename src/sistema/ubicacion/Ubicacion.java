b7348a77ee3164e8359e4f007588fb0db298d8c1
AcoMat <aaco.maty@gmail.com>
2023-05-31 16:27:48 -0300

    metodos de la clase Ubicacion

--- src/sistema/ubicacion/Ubicacion.java
+++ src/sistema/ubicacion/Ubicacion.java
@@ -1,8 +1,25 @@
 package sistema.ubicacion;
 
+import sistema.muestras.Muestra;
+
+import java.util.ArrayList;
+
 public class Ubicacion {
 	
 	double latitud;
 	double longitud;
+
+	public int distanciaHasta(Ubicacion ubi){
+		//TODO, teorema de pitagoras
+		return 0;
+	}
+
+	public ArrayList<Ubicacion> aquellasAMenosDe1Km(ArrayList<Ubicacion> ubicaciones){
+		//TODO, filtrar aquellas q this.distanciaHasta < 1
+	}
+
+	public ArrayList<Muestra> muestrasAMenosDeXKm(int kms){
+		//TODO
+	}
 	
 }
