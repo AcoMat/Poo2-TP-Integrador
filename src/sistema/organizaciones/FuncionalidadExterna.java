b770e5507080f03cb75e2598cd7f6b112f06c48c
AcoMat <aaco.maty@gmail.com>
2023-05-31 16:50:21 -0300

    Agrego interfaz "FuncionalidadExterna"

---
+++ src/sistema/organizaciones/FuncionalidadExterna.java
@@ -0,0 +1,5 @@
+package sistema.organizaciones;
+
+public interface FuncionalidadExterna {
+    public void nuevoEvento(<Organización>, <zonaDeCobertura>, <Muestra>);
+}
--- src/sistema/organizaciones/Organizacion.java
+++ src/sistema/organizaciones/Organizacion.java
@@ -1,10 +1,34 @@
 package sistema.organizaciones;
 
 import sistema.ubicacion.Ubicacion;
+import sistema.ubicacion.ZonaDeCobertura;
+
+import java.util.ArrayList;
 
 public class Organizacion {
     private Ubicacion ubicacion;
     private TipoDeOrg tipoDeLaOrg;
     private int cantEmpleados;
+
+    private FuncionalidadExterna seCargoUnaNuevaMuestra;
+    private FuncionalidadExterna seValidoUnaMuestra;
+
+    private ArrayList<ZonaDeCobertura> zonasSubscritas;
+
+    public void suscribirseAZona(ZonaDeCobertura zona){
+        //observer
+    }
+
+    public void deSuscribirseDe(ZonaDeCobertura zona){
+        //TODO
+    }
+
+    public void eventoNuevaMuestra(){
+
+    }
+
+    public void eventoNuevaValidacion(){
+
+    }
 
 }
