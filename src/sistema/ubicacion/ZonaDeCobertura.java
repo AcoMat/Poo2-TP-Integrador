4a139aec5c03a864656d41419756f0abe8bd45f1
AcoMat <aaco.maty@gmail.com>
2023-05-31 16:36:53 -0300

    Inicializo la clase ZonaDeCobertura

--- src/sistema/ubicacion/ZonaDeCobertura.java
+++ src/sistema/ubicacion/ZonaDeCobertura.java
@@ -1,5 +1,23 @@
 package sistema.ubicacion;
 
+import sistema.muestras.Muestra;
+
+import java.util.ArrayList;
+
 public class ZonaDeCobertura {
+    private Ubicacion epicentro;
+    private Integer radio;
+    private String nombre;
+
+    private ArrayList<Muestra> muestrasReportadas;
+
+    //  Metodos
+
+    public ArrayList<ZonaDeCobertura> zonasQueSeSolapan(){
+        //TODo
+    }
+
+
+
 
 }
