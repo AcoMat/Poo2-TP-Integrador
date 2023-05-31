677230c27f2f17f5e3bdb5fa52b14ef24fb078ac
Eduardo73Martinez <tinchops4@hotmail.com>
2023-05-31 18:00:44 -0300

    Funcionalidades del buscador

--- src/sistema/buscador/Buscador.java
+++ src/sistema/buscador/Buscador.java
@@ -1,15 +1,53 @@
 package sistema.buscador;
 
 import sistema.muestras.Muestra;
+import sistema.sistemaDeVotos.Opinion;
+import sistema.sistemaDeVotos.TipoDeVoto;
+import sistema.sistemaDeVotos.validacion.Validacion;
 
+import java.util.Date;
+import java.util.Optional;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Buscador {
-    //Proposito: se encarga de guardar todas las muestras para su posterior busqueda
+	// Proposito: se encarga de guardar todas las muestras para su posterior
+	// busqueda
 
-    private ArrayList<Muestra> muestasTotalesDelSys;
+	private ArrayList<Muestra> muestrasTotalesDelSys;
+	private Comparator<Opinion> comparator = Comparator.comparing(Opinion::getFecha);
 
-    public void nuevaMuestraEnSistema(Muestra m){
-        muestasTotalesDelSys.add(m);
+	public void nuevaMuestraEnSistema(Muestra m) {
+		muestrasTotalesDelSys.add(m);
+	}
+
+	// buscar muestras por la fecha de creacion
+	public ArrayList<Muestra> muestraCreadaEnLaFecha(Date fecha) {
+		return (ArrayList<Muestra>) muestrasTotalesDelSys.stream().filter(s -> s.getFecha() == fecha);
+	}
+
+	// consultar tenemos 2 arrrays para opiniones
+	public Opinion ultimaOpinionBasico(Muestra muestra) {
+		return muestra.getHandler().getOpinionesBasicos().stream().max(comparator).get();
+	}
+
+	public Opinion ultimaOpinionExperto(Muestra muestra) {
+		return muestra.getHandler().getOpinionesExpertos().stream().max(comparator).get();
+	}
+
+	public Date ultimaFechaDeVotacionBasico() {
+    	return muestrasTotalesDelSys.stream().forEach(s-> this.ultimaOpinionBasico(s)).max(comparator).get()comparator.;
+    	
     }
+
+	// Busqueda por la especie 
+	public ArrayList<Muestra> muestrasConInsecto(TipoDeVoto insecto) {
+		return (ArrayList<Muestra>) muestrasTotalesDelSys.stream().filter(s -> s.getEspecie() == insecto);
+	}
+	
+	// Busqueda por la validacion (falta completar la clase validacion con la muestra)
+	public ArrayList<Muestra> nivelValidacion(Validacion validacionMuestra){
+    	return (ArrayList<Muestra>) muestrasTotalesDelSys.stream().filter(s-> s.getEstado()== ValidacionMuestra));
+    }
+
 }
