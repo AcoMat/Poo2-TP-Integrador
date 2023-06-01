bdd98776c1f3275c8dad93795bc045588579f68c
Eduardo73Martinez <tinchops4@hotmail.com>
2023-05-31 22:04:34 -0300

    creo el test del buscador todavia sin la implementacion

---
+++ src/sistema/buscador/TestBuscador.java
@@ -0,0 +1,39 @@
+package sistema.buscador;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestBuscador {
+
+	@Test
+	void testNuevaMuestraEnSistema() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMuestraCreadaEnLaFecha() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMuestrasConInsecto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNivelValidacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBuscadorGeneral() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testObject() {
+		fail("Not yet implemented");
+	}
+
+}
