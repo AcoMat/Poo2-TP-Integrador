a9c6066914b6ea3c74b505ae48ad5ddf03a55ae4
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2023-06-02 17:26:41 -0300

    Correción clase Validada

--- src/sistema/sistemaDeVotos/validacion/Validada.java
+++ src/sistema/sistemaDeVotos/validacion/Validada.java
@@ -2,17 +2,32 @@ package sistema.sistemaDeVotos.validacion;
 
 import sistema.sistemaDeVotos.ManejadorDeVotos;
 import sistema.sistemaDeVotos.Opinion;
+import sistema.sistemaDeVotos.TipoDeVoto;
 
-public class Validada extends Validacion{
+public class Validada extends EstadoValidacion {
 
-    Validada(ManejadorDeVotos h){
-        super(h);
-    }
+	TipoDeVoto votoValidado;
 
-    @Override
-    protected void registrarOpinion(Opinion o) {
-        System.out.println("esta muestra ya esta validada, no se puede votar");
-    }
+	public Validada(TipoDeVoto votoGanador) {
+		votoValidado = votoGanador;
+	}
 
+	// Booleanos a refactorizar
+	public Boolean permiteVotoBasico() {
+		return false;
+	}
+
+	public Boolean permiteVotoExperto() {
+		return false;
+	}
+
+	public EstadoValidacion cambioDeEstado(ManejadorDeVotos manejadorDeVotos, Opinion opinion) {
+		return this;
+	}
+
+	// Resultado Actual
+	public TipoDeVoto resultadoActual(ManejadorDeVotos manejadorDeVotos) {
+		return votoValidado;
+	}
 
 }
