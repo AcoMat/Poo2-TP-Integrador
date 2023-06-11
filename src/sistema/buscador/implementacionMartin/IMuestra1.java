c4a39251dc1276e511ed96d18f4735406950c2d5
Eduardo73Martinez <tinchops4@hotmail.com>
2023-06-11 16:47:30 -0300

    Cambio la clase abstracta por una interface

--- src/sistema/buscador/implementacionMartin/IMuestra1.java
+++ src/sistema/buscador/implementacionMartin/IMuestra1.java
@@ -2,22 +2,15 @@ package sistema.buscador.implementacionMartin;
 
 import java.util.Date;
 
-public abstract class IMuestra1 {
-	public  ITipoDeVoto1 getEstado() {
-		return null;
-	}
-	public ITipoDeVoto1 getEspecieEstadoActual() {
-		return null;
-	}
-	public IManejadorVotos1 getManejadorVotos() {
-		return null;
-	}
-	public Date getFecha() {
-		return null;
-	}
-	public Date getFechaUltimaOpinion() {
-		return null;}
-	
+public interface IMuestra1 {
 
-	
+	public ITipoDeVoto1 getEstado();
+
+	public ITipoDeVoto1 getEspecieEstadoActual();
+
+	public IManejadorVotos1 getManejadorVotos();
+
+	public Date getFecha();
+
+	public Date getFechaUltimaOpinion();
 }
