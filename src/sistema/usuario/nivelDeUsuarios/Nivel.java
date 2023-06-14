f03e913681846512524eb526ccebd880326d3a25
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2023-06-14 20:39:17 -0300

    Corrección en nombre de clases

--- src/sistema/usuario/nivelDeUsuarios/Nivel.java
+++ src/sistema/usuario/nivelDeUsuarios/Nivel.java
@@ -1,6 +1,7 @@
 package sistema.usuario.nivelDeUsuarios;
 
 import sistema.sistemaDeVotos.Opinion;
+
 import sistema.sistemaDeVotos.TipoDeVoto;
 import sistema.ubicacion.Ubicacion;
 import sistema.muestras.*;
@@ -9,7 +10,7 @@ import sistema.usuario.Usuario;
 public abstract class Nivel {
 	public abstract void opinar(Usuario usuario, Muestra muestra, TipoDeVoto voto);
 
-	public void enviarMuestra(IUsuario usuario, ITipoDeVoto2 especie, String fotoURL, IUbicacion ubicacion) {
+	public void enviarMuestra(Usuario usuario, TipoDeVoto especie, String fotoURL, Ubicacion ubicacion) {
 		Muestra nuevaMuestra = new Muestra(especie, fotoURL, usuario, ubicacion);
 		usuario.guardarMuestra(nuevaMuestra);
 	};
