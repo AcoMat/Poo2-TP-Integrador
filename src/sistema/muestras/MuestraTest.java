33215cbb7ce221f9d3d0b9ab0994cf1fee5d4aba
Eduardo73Martinez <tinchops4@hotmail.com>
2023-06-14 12:15:56 -0300

    correccion de un error en test Buscador

---
+++ src/sistema/muestras/MuestraTest.java
@@ -0,0 +1,89 @@
+package sistema.muestras;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MuestraTest {
+
+	@Test
+	void testMuestra() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFotoURL() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAutor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUbicacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetManejadorVotos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFecha() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEspecie() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTodasLasOpiniones() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUltimaVotacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFotoURL() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAutor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetUbicacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFechaUltimaOpinion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testResultadoActual() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFecha() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEspecie() {
+		fail("Not yet implemented");
+	}
+
+}
