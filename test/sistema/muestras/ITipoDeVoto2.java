7bf5462f945f4564e7ffc65a9fc5c34bbd87e542
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2023-06-14 18:22:28 -0300

    Se copiaron archivos de test a la carpeta test

---
+++ test/sistema/muestras/ITipoDeVoto2.java
@@ -0,0 +1,11 @@
+package sistema.muestras;
+
+public enum ITipoDeVoto2 {
+	Vinchuca_Infestans,
+	Vinchuca_Sordida,
+	Vinchuca_Guasayana,
+	Chinche_Foliada,
+	Phtia_Chinche,
+	Ninguna,
+	Imagen_poco_clara
+}
