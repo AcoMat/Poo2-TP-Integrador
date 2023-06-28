4dd59f0ed071e665b91fbb5956132474cdd648cc
Eduardo73Martinez <tinchops4@hotmail.com>
2023-06-28 18:28:13 -0300

    arreglo de las opiniones, ahora no guardan el nombre ahora guarda la clase usuario que opinó

--- src/sistema/usuario/nivelDeUsuarios/Basico.java
+++ src/sistema/usuario/nivelDeUsuarios/Basico.java
@@ -8,7 +8,7 @@ import sistema.usuario.Usuario;
 public class Basico extends Nivel {
 
 	public void opinar(Usuario usuario, Muestra muestra, TipoDeVoto voto) {
-		Opinion nuevaOpinion = new Opinion(voto, usuario.getUserName());
+		Opinion nuevaOpinion = new Opinion(voto, usuario);
 		muestra.getManejadorVotos().agregarOpinionBasica(nuevaOpinion);
 		usuario.guardarOpinion(muestra, nuevaOpinion);
 	};
--- src/sistema/usuario/nivelDeUsuarios/Experto.java
+++ src/sistema/usuario/nivelDeUsuarios/Experto.java
@@ -8,7 +8,7 @@ import sistema.usuario.Usuario;
 public class Experto extends Nivel {
 
 	public void opinar(Usuario usuario, Muestra muestra, TipoDeVoto voto) {
-		Opinion nuevaOpinion = new Opinion(voto, usuario.getUserName());
+		Opinion nuevaOpinion = new Opinion(voto, usuario);
 		muestra.getManejadorVotos().agregarOpinionExperta(nuevaOpinion);
 		usuario.guardarOpinion(muestra, nuevaOpinion);
 	};
