7f1daaf95556435755b5b0e961689b94e4af6424
Eduardo73Martinez <tinchops4@hotmail.com>
2023-06-28 00:44:33 -0300

    Creo el buscador conjuntivo (or)

---
+++ src/sistema/buscadorV3/BuscadorConjuntivo.java
@@ -0,0 +1,25 @@
+package sistema.buscadorV3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sistema.muestras.Muestra;
+
+public class BuscadorConjuntivo extends CombinadorDeBusqueda {
+
+	public BuscadorConjuntivo(IBuscador primerTipoDeFiltro, IBuscador segundoTipoDeFiltro,
+			ArrayList<Muestra> primeraBusqueda, ArrayList<Muestra> segundaBusqueda) {
+		super(primerTipoDeFiltro, segundoTipoDeFiltro, primeraBusqueda, segundaBusqueda);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<Muestra> tipoDeCombinacion(List<Muestra> muestra1, List<Muestra> muestra2) {
+		// TODO Auto-generated method stub
+		List<Muestra> resultado = new ArrayList<Muestra>();
+		resultado.addAll(muestra1);
+		resultado.retainAll(muestra2);
+		return resultado;
+	}
+
+}
