35fde3cad328b743b20f1ffdb1c001aa0ef5c2d0
Eduardo73Martinez <tinchops4@hotmail.com>
2023-06-28 19:02:46 -0300

    creo un metodo para la condicion de si esta en la zona (ZOnaDeCobertura)

--- src/sistema/ubicacion/ZonaDeCobertura.java
+++ src/sistema/ubicacion/ZonaDeCobertura.java
@@ -6,72 +6,74 @@ import sistema.organizaciones.Organizacion;
 import java.util.ArrayList;
 
 public class ZonaDeCobertura {
-    private Ubicacion epicentro;
-    private double radio;
-    private String nombre;
+	private Ubicacion epicentro;
+	private double radio;
+	private String nombre;
 
-    private ArrayList<Muestra> muestrasReportadas = new ArrayList<Muestra>();
+	private ArrayList<Muestra> muestrasReportadas = new ArrayList<Muestra>();
 
-    private ArrayList<Organizacion> organizacionesSuscritas = new ArrayList<Organizacion>();
+	private ArrayList<Organizacion> organizacionesSuscritas = new ArrayList<Organizacion>();
 
-    public Ubicacion getEpicentro() {
-        return epicentro;
-    }
+	public Ubicacion getEpicentro() {
+		return epicentro;
+	}
 
-    public double getRadio() {
-        return radio;
-    }
+	public double getRadio() {
+		return radio;
+	}
 
-    //
+	//
 
-    public ZonaDeCobertura(Ubicacion epicentro, double radio,String nombre){
-        this.epicentro = epicentro;
-        this. radio = radio;
-        this.nombre = nombre;
-    }
+	public ZonaDeCobertura(Ubicacion epicentro, double radio, String nombre) {
+		this.epicentro = epicentro;
+		this.radio = radio;
+		this.nombre = nombre;
+	}
 
-    //  Metodos
+	// Metodos
 
-    public void agregarSiEstaEnLaZona(Muestra muestra){
-        if(this.epicentro.distanciaHasta((Ubicacion) muestra.getUbicacion()) < this.radio){
-            this.nuevaMuestraEnLaZona(muestra);
-        }
-    }
+	public void agregarSiEstaEnLaZona(Muestra muestra) {
+		if (estaEnCobertura(muestra)) {
+			this.nuevaMuestraEnLaZona(muestra);
+		}
+	}
 
-    public void nuevaMuestraEnLaZona(Muestra muestra){
-        this.muestrasReportadas.add(muestra);
-        for (Organizacion org:organizacionesSuscritas) {
-            org.eventoNuevaMuestra(muestra, this);
-        }
-    }
+	private boolean estaEnCobertura(Muestra muestra) {
+		return this.epicentro.distanciaHasta((Ubicacion) muestra.getUbicacion()) < this.radio;
+	}
 
+	public void nuevaMuestraEnLaZona(Muestra muestra) {
+		this.muestrasReportadas.add(muestra);
+		for (Organizacion org : organizacionesSuscritas) {
+			org.eventoNuevaMuestra(muestra, this);
+		}
+	}
 
+	public void nuevaValidacion(Muestra muestra) {
+		if (estaEnCobertura(muestra)) {
+			for (Organizacion org : organizacionesSuscritas) {
+				org.eventoNuevaValidacion(muestra, this);
+			}
+		}
+	}
 
-    public void nuevaValidacion(Muestra muestra){
-        if(this.epicentro.distanciaHasta((Ubicacion) muestra.getUbicacion()) < this.radio) {
-            for (Organizacion org : organizacionesSuscritas) {
-                org.eventoNuevaValidacion(muestra, this);
-            }
-        }
-    }
+	public void suscribirA(Organizacion org) {
+		organizacionesSuscritas.add(org);
+	}
 
-    public void suscribirA(Organizacion org) {
-        organizacionesSuscritas.add(org);
-    }
+	public void desuscribirA(Organizacion org) {
+		organizacionesSuscritas.remove(org);
+	}
 
-    public void desuscribirA(Organizacion org) {
-        organizacionesSuscritas.remove(org);
-    }
+	public boolean seSolapaCon(ZonaDeCobertura zc) {
+		return (this.epicentro.distanciaHasta(zc.getEpicentro())) < this.radio;
+	}
 
-    public boolean seSolapaCon(ZonaDeCobertura zc){
-        return (this.epicentro.distanciaHasta(zc.getEpicentro())) < this.radio;
-    }
+	public ArrayList<Muestra> getMuestrasReportadas() {
+		return muestrasReportadas;
+	}
 
-    public ArrayList<Muestra> getMuestrasReportadas() {
-        return muestrasReportadas;
-    }
-
-    public ArrayList<Organizacion> getOrganizacionesSuscritas() {
-        return organizacionesSuscritas;
-    }
+	public ArrayList<Organizacion> getOrganizacionesSuscritas() {
+		return organizacionesSuscritas;
+	}
 }
